d011aff71b82cd873c7ba8313be610b2586666a2
Elvin Mucaj <emuco7@yahoo.com>
2023-06-12 12:02:29 +0200

    Update SqlConnectionString.cs

--- Modules/SqlConnectionString.cs
+++ Modules/SqlConnectionString.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 class SqlConnectionString
 {
-    private static string datasource = "aida-pc";
-    private static string[] database = { "EPG" };
-    private static string username = "seven";
-    private static string password = "root@123";
+    private static string datasource = "";
+    private static string[] database = { "" };
+    private static string username = "";
+    private static string password = "";
     private static string cstring = "";
 
     public static string Get(int i)
